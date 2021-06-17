bcd761ec11c18027444d25daf13dc79d877eaae8
wiotrek <piotrwaszak97@gmail.com>
2021-06-17 17:43:16 +0200

    close #1

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './authorization/auth.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', component: MainComponent },
+      { path: 'logowanie', component: AuthComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

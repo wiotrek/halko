fb03fae035951f26c6bc57c1fa11f78c61d25586
wiotrek <piotrwaszak97@gmail.com>
2021-06-19 14:29:29 +0200

    logging html structure creating

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AuthComponent } from './authorization/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/authorization/auth.component.ts
+++ web/src/app/authorization/auth.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { faHandPeace } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
     styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {}
+export class AuthComponent {
+    faHandPeace = faHandPeace;
+}

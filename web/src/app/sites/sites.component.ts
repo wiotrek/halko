6626d246e35f2f8297c10aa5529fd7e9b05e275e
wiotrek <piotrwaszak97@gmail.com>
2021-06-25 19:32:40 +0200

    #9 starting nav component and added logo

--- web/src/app/sites/sites.component.ts
+++ web/src/app/sites/sites.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-sites',
-    template: `
-    <app-nav></app-nav>
-    `, styleUrls: ['./sites.component.scss']
+    templateUrl: './sites.component.html',
+    styleUrls: ['./sites.component.scss']
 })
 export class SitesComponent {}

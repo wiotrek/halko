a6997975a098f838c55f52455aeb74d6a4e94172
Kipoderax <sebastian.rajczyk@wp.pl>
2021-07-01 19:18:47 +0200

    close #15

---
+++ Api/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Components.Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}

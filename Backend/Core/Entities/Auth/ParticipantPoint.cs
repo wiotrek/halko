9e0e21f79825f9d76b8a906abe6ad32a76fe047a
Kipoderax <sebastian.rajczyk@wp.pl>
2021-07-03 21:06:00 +0200

    close #16

---
+++ Backend/Core/Entities/Auth/ParticipantPoint.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities.Auth
+{
+    /// <summary>
+    /// The member of specific point
+    /// </summary>
+    public class ParticipantPoint : BaseEntity
+    {
+        public string Initial { get; set; }
+    }
+}

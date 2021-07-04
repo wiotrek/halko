a42cc69dc19d5d0f5011dd4571e21ed7351b941a
Kipoderax <sebastian.rajczyk@wp.pl>
2021-07-04 18:46:02 +0200

    #26 Specification pattern

---
+++ Backend/Core/Specifications/ISpecification.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    /// <summary>
+    /// Requirements for retrieving data from T
+    /// </summary>
+    /// <typeparam name="T">The entity</typeparam>
+    public interface ISpecification<T>
+    {
+        /// <summary>
+        /// Data filter for specific entity
+        /// </summary>
+        Expression<Func<T, bool>> Criteria { get; }
+        
+        /// <summary>
+        /// Join list of entities being part of T 
+        /// </summary>
+        List<Expression<Func<T, object>>> Includes { get; }
+        
+        /// <summary>
+        /// Data sorting in ascending way
+        /// </summary>
+        Expression<Func<T, object>> OrderBy { get; }
+        
+        /// <summary>
+        /// Data sorting in descending way
+        /// </summary>
+        Expression<Func<T, object>> OrderByDescending { get; }
+    }
+}

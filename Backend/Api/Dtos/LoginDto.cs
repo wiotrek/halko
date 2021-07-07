a2939b112436ffc5b627ebbf80cc078d62548cc2
Kipoderax <sebastian.rajczyk@wp.pl>
2021-07-07 19:33:36 +0200

    close #20

---
+++ Backend/Api/Dtos/LoginDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Dtos
+{
+    /// <summary>
+    /// Usage while user is login
+    /// </summary>
+    public class LoginDto
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}

5b488331780db7c749e40110bfa23d59caa5f721
wiotrek <piotrwaszak97@gmail.com>
2021-07-10 16:02:08 +0200

    #28 searcher is close to done

--- web/src/app/sites/phones/phones-seacher/phones-seacher.component.ts
+++ web/src/app/sites/phones/phones-seacher/phones-seacher.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { faArrowUp, faArrowDown, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { Dictionary } from 'src/app/shared/models/dictionary.model';
+
 
 @Component({
     selector: 'app-phones-seacher',
@@ -6,4 +9,31 @@ import { Component } from '@angular/core';
     styleUrls: ['./phones-seacher.component.scss']
 })
 
-export class PhonesSeacherComponent {}
+export class PhonesSeacherComponent {
+    faArrowUp = faArrowUp;
+    faArrowDown = faArrowDown;
+
+    iconsToValue: Dictionary<IconDefinition> = {
+        up: faArrowUp,
+        down: faArrowDown,
+    };
+
+    valuesToSorted: { name: string, vector: string }[] =  [
+        { name: 'cena', vector: 'up'},
+        { name: 'cena', vector: 'down'},
+        { name: 'nazwa', vector: 'up'},
+        { name: 'nazwa', vector: 'down'},
+    ];
+
+    defaultSorted = { name: 'cena', vector: 'up' };
+
+    state = 'all';
+
+    pointsNames = [
+        'Wszystkie',
+        'Karuzela Września',
+        'Kaufland Poznań'
+    ];
+
+    defaultPoint = 'Karuzela Września';
+}

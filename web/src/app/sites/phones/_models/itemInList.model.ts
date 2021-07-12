0a73d79e0b62dd925c558fc6d2fba98865723a29
wiotrek <piotrwaszak97@gmail.com>
2021-07-12 13:05:07 +0200

    #28 done element in list html/scss

---
+++ web/src/app/sites/phones/_models/itemInList.model.ts
@@ -0,0 +1,11 @@
+export interface ItemInList {
+    ind: number;
+    brand: string;
+    model: string;
+    imei: string;
+    color: string;
+    place: string;
+    state: string;
+    bought: number;
+    price: number;
+}
--- web/src/app/sites/phones/phones-list/phones-list.component.ts
+++ web/src/app/sites/phones/phones-list/phones-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { faEdit } from '@fortawesome/free-regular-svg-icons';
+import { ItemInList } from '../_models/itemInList.model';
 
 @Component({
     selector: 'app-phones-list',
@@ -6,4 +8,21 @@ import { Component } from '@angular/core';
     styleUrls: ['./phones-list.component.scss']
 })
 
-export class PhonesListComponent {}
+export class PhonesListComponent {
+    elInList: ItemInList;
+    faEdit = faEdit;
+
+    constructor() {
+        this.elInList = {
+            ind: 1,
+            brand: 'Apple',
+            model: 'Iphone 7',
+            imei: '123123123123',
+            color: 'Space gray',
+            place: 'Karuzela Września',
+            state: 'Nowy',
+            bought: 700,
+            price: 1300
+        };
+    }
+}

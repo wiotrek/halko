069e8673960d81223acea67b34a5889b7773d302
wiotrek <piotrwaszak97@gmail.com>
2021-07-14 15:18:25 +0200

    #28 changer for phones-details

--- web/src/app/sites/phones/phones-item/phones-details/phones-details.component.ts
+++ web/src/app/sites/phones/phones-item/phones-details/phones-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ItemInList } from '../../_models/itemInList.model';
 
 @Component({
@@ -6,15 +6,52 @@ import { ItemInList } from '../../_models/itemInList.model';
     templateUrl: 'phones-details.component.html',
     styleUrls: ['phones-details.component.scss']
 })
-export class PhonesDetailsComponent {
+export class PhonesDetailsComponent implements OnInit{
     @Input() elInList: ItemInList;
 
-    someModeIsActive = false;
+    // these elements will replacement each other
+    @ViewChild(
+        'viewAndEditPhone', {static: true}
+    ) viewAndEditPhone: TemplateRef<any> | null = null;
 
+    @ViewChild(
+        'soldPhone', {static: true}
+    ) soldPhone: TemplateRef<any> | null = null;
+
+    @ViewChild(
+        'sendPhone', {static: true}
+    ) sendPhone: TemplateRef<any> | null = null;
+
+    // block which any elements will replace
+    showBlock: TemplateRef<any>|null = null;
+    show = true;
+
+    undoButtonActive = false;
     editMode = false;
 
-    someModeIsActiveToggle = () => {
-        this.editMode = !this.editMode;
-        this.someModeIsActive = !this.someModeIsActive;
+    ngOnInit(): void {
+        this.showBlock = this.viewAndEditPhone;
+    }
+
+    switchBlock(nameOfBlock: string): void {
+
+        switch (nameOfBlock) {
+            case 'viewAndEditPhone':
+                this.showBlock = this.viewAndEditPhone;
+                this.editMode = true;
+                break;
+            case 'soldPhone':
+                this.showBlock = this.soldPhone;
+                break;
+            case 'sendPhone':
+                this.showBlock = this.sendPhone;
+                break;
+            case 'back':
+                this.showBlock = this.viewAndEditPhone;
+                this.editMode = false;
+                break;
+        }
+
+        this.undoButtonActive = !this.undoButtonActive;
     }
 }

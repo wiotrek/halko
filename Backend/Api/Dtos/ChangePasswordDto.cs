256ab5de35172e9e0b8be32a8629812ef0f6ab48
Kipoderax <sebastian.rajczyk@wp.pl>
2021-07-20 20:30:22 +0200

    close #44

---
+++ Backend/Api/Dtos/ChangePasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Dtos
+{
+    public class ChangePasswordDto
+    {
+        public string Login { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}

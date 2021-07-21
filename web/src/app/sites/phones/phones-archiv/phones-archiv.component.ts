33568b22720fac959ab1cb0076b65465f8c0cfea
wiotrek <piotrwaszak97@gmail.com>
2021-07-21 16:42:46 +0200

    start  #39

--- web/src/app/sites/phones/phones-archiv/phones-archiv.component.ts
+++ web/src/app/sites/phones/phones-archiv/phones-archiv.component.ts
@@ -1,7 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhoneFields } from '../_dictionary/phone-fields.dictionary';
+import { ItemInListModel } from '../_models/item-in-list.model';
 
 @Component({
     selector: 'app-phones-archiv',
-    template: 'dsaup'
+    templateUrl: './phones-archiv.component.html',
+    styleUrls: ['./phones-archiv.component.scss']
 })
-export class PhonesArchivComponent {}
+export class PhonesArchivComponent implements OnInit{
+    phonesList: ItemInListModel[] = [
+        {
+            brand: 'Apple',
+            model: 'Iphone 7',
+            imei: '123123123123',
+            color: 'Space gray',
+            place: 'Karuzela Września',
+            state: 'Nowy',
+            comments: 'wszystko ladnie',
+            dateBought: '2021-03-02',
+            bought: 700,
+            price: 1300
+        },
+        {
+            brand: 'Samsung',
+            model: 'S7',
+            imei: '123123123123',
+            color: 'Blue',
+            place: 'Karuzela Września',
+            state: 'Używany',
+            comments: 'nic nie ladnie',
+            dateBought: '2021-03-02',
+            bought: 7000,
+            price: 2000
+        },
+        {
+            brand: 'Huawei',
+            model: 'P30 pro lite duo',
+            imei: '123123123123',
+            color: 'star red spophhie',
+            place: 'Karuzela Września',
+            state: 'Nowy',
+            dateBought: '2021-03-02',
+            bought: 30,
+            price: 100
+        },
+        {
+            brand: 'Xiaomi',
+            model: 'Pro lite duo phone',
+            imei: '123123123123',
+            color: 'yellow green blue',
+            place: 'Karuzela Września',
+            state: 'Używany',
+            dateBought: '2021-03-02',
+            bought: 500,
+            price: 300
+        }
+
+    ];
+
+    phonesField: string[];
+
+
+    ngOnInit(): void {
+        this.phonesField = [...Object.keys(this.phonesList[0])];
+
+        this.phonesField
+        .splice(
+            this.phonesField.indexOf('comments'), 1
+        )
+        .splice(
+            this.phonesField.indexOf('dateBought'), 1
+        );
+
+    }
+
+
+}
---
+++ web/src/app/sites/phones/phones-item-raw/phones-item-raw.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-phones-item-raw',
+    templateUrl: './phones-item-raw.component.html',
+    styleUrls: ['./phones-item-raw.component.scss']
+})
+export class PhonesItemRawComponent {}

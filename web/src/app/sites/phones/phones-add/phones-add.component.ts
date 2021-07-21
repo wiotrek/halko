924bcd37c4b74714d9afbc7c3d4f5e7fdf01ae50
wiotrek <piotrwaszak97@gmail.com>
2021-07-21 11:06:00 +0200

    #38 started

--- web/src/app/sites/phones/phones-add/phones-add.component.ts
+++ web/src/app/sites/phones/phones-add/phones-add.component.ts
@@ -1,7 +1,53 @@
 import { Component } from '@angular/core';
 
+interface Field {
+    categoryName: string;
+    polishName: string;
+    isNumber: boolean;
+}
+
 @Component({
     selector: 'app-phones-add',
-    template: 'dsad'
+    templateUrl: './phones-add.component.html',
+    styleUrls: ['./phones-add.component.scss']
 })
-export class PhonesAddComponent {}
+export class PhonesAddComponent {
+
+    fields: Field[] = [
+        {
+            categoryName: 'brand',
+            polishName: 'Producent',
+            isNumber: false
+        },
+        {
+            categoryName: 'model',
+            polishName: 'Model',
+            isNumber: false
+        },
+        {
+            categoryName: 'imei',
+            polishName: 'Imei',
+            isNumber: true
+        },
+        {
+            categoryName: 'color',
+            polishName: 'Kolor',
+            isNumber: false
+        },
+        {
+            categoryName: 'comments',
+            polishName: 'Uwagi',
+            isNumber: false
+        },
+        {
+            categoryName: 'bought',
+            polishName: 'Cena zakupu',
+            isNumber: true
+        },
+        {
+            categoryName: 'price',
+            polishName: 'Cena',
+            isNumber: true
+        }
+    ];
+}

19f642ace1807e89a62a6031ddd0a0486cdb61cb
wiotrek <piotrwaszak97@gmail.com>
2021-07-22 15:47:43 +0200

    close #39 --delete raw dir

--- web/src/app/sites/phones/phones-item-raw/phones-item-raw.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-phones-item-raw',
-    templateUrl: './phones-item-raw.component.html',
-    styleUrls: ['./phones-item-raw.component.scss']
-})
-export class PhonesItemRawComponent {}

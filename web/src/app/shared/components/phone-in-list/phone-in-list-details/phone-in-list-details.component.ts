15eec85db51e64f824be78b4cc877c3b242f9205
wiotrek <piotrwaszak97@gmail.com>
2021-09-05 21:37:13 +0200

    close #173

--- web/src/app/shared/components/phone-in-list/phone-in-list-details/phone-in-list-details.component.ts
+++ web/src/app/shared/components/phone-in-list/phone-in-list-details/phone-in-list-details.component.ts
@@ -14,6 +14,7 @@ import {
 import { PhoneInListDetailsCptsType } from 'src/app/shared/array/phone-in-list-details-cpts.array';
 import { PhoneInListType } from 'src/app/shared/models-union/phone-in-list.type';
 import { PhoneFieldsModel } from 'src/app/shared/models/phone-fields.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-phone-in-list-details',
@@ -24,11 +25,21 @@ export class PhoneInListDetailsComponent implements AfterViewInit {
     @Input() deviceFields: PhoneFieldsModel[];
     @Input() elInList: PhoneInListType;
 
+    // if may edit fields set true
+    @Input() isExistEditMode?: boolean;
+
     // variable for which will be assign name component
     @Input() componentWillUsing?: Type<PhoneInListDetailsCptsType>;
     @Output() componentBeingUsingOutput: EventEmitter<any> = new EventEmitter<any>();
 
+    // if fields will update then sending old version, and new version
+    @Output() updateDetails: EventEmitter<{
+        update: NgForm, elInList: PhoneInListType
+    }> = new EventEmitter<{update: NgForm, elInList: PhoneInListType}>();
+
     @ViewChild('optComponent', { read: ViewContainerRef }) optComponent: ViewContainerRef;
+
+    editModeActive = false;
 
     constructor(
         private injector: Injector,
@@ -37,7 +48,26 @@ export class PhoneInListDetailsComponent implements AfterViewInit {
     ) {}
 
     ngAfterViewInit(): void {
+        this.createOptionalComponent();
+    }
 
+    updateElement(update: NgForm): void {
+        const response: {
+            update: NgForm,
+            elInList: PhoneInListType
+        } = {
+            update,
+            elInList: this.elInList
+        };
+
+        this.updateDetails.emit(response);
+    }
+
+    toggleEditModeActive(): void {
+        this.editModeActive = !this.editModeActive;
+    }
+
+    private createOptionalComponent(): void {
         if (this.componentWillUsing) {
 
             // prepare choosen component to display on template

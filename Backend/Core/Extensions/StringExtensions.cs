9b9a2b5e3eb7f51587d411ada499ead11cf0e69f
Kipoderax <sebastian.rajczyk@wp.pl>
2021-09-05 18:12:29 +0200

    #161 extend params filter

---
+++ Backend/Core/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FirstLetterToUpper( this string input )
+        {
+            return string.IsNullOrEmpty ( input )
+                ? string.Empty
+                : string.Concat ( input.Select ( ( currentChar, index ) =>
+                    index == 0 
+                        ? char.ToUpper ( currentChar ) 
+                        : currentChar ) );
+        }
+    }
+}

9a49f6705177f7ecac2e1b9c56b34f387990a978
wiotrek <piotrwaszak97@gmail.com>
2021-09-09 20:59:04 +0200

    close #171

--- web/src/app/sites/prices/view/prices.component.ts
+++ web/src/app/sites/prices/view/prices.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { PricesFieldsArray } from '../prices-fields.array';
+import { PricesFieldsArray } from './_models/prices-fields.array';
 import { PricesModel } from '../../../shared/models/prices.model';
-import {SearcherPatternModel} from '../../../shared/components/searcher/_models/searcher-pattern.model';
-import {SearcherModel} from '../../../shared/models/searcher.model';
+import { SearcherPatternModel } from '../../../shared/components/searcher/_models/searcher-pattern.model';
+import { SearcherModel } from '../../../shared/models/searcher.model';
 import { PricesService } from '../prices.service';
+import { PricesApiGetPagModel } from '../_models-pagination/prices-api-get-pag.model';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-prices',
@@ -11,41 +14,77 @@ import { PricesService } from '../prices.service';
     styleUrls: ['prices.component.scss']
 })
 export class PricesComponent implements OnInit {
-    pricesFieldsArray = PricesFieldsArray;
-    isFlexStart = true;
     headline = 'Cennik';
 
+    // main list
+    phonePrices: PricesModel[];
+
+    // amount getting elements from api
+    phonesAmount: number;
+
+    // field unnecessery to generate component
+    pricesFieldsArray = PricesFieldsArray;
+
+    // elements in phone in list will display from left to right,
+    // but not in center
+    isFlexStart = true;
+
+    // choice elements on searcher component
     searcherPattern: SearcherPatternModel = {
         sorting: false,
         filterNewUsed: false,
         filterPoints: false
     };
 
+    // searcher property will for getting from api
+    // choice filter, from whichers will be create params
     searcher: SearcherModel = {
         pointName: '',
         searchName: '',
-        state: ''
+        state: '',
+        pageIndex: 1,
+        pageSize: 10,
     };
 
-    phonePrices: PricesModel[];
-
     constructor(
-        private pricesService: PricesService
+        private pricesService: PricesService,
+        private toastr: ToastrService
     ) {}
 
     ngOnInit(): void {
-        this.phonePrices = this.pricesService.getPricesList();
-        this.pricesService.pricesList$.subscribe(
-            res => this.phonePrices = res
-        );
+        this.getPrices(this.searcher);
     }
 
     searchNameFilter(name: string): void {
         this.searcher.searchName = name;
-        this.pricesService.pricesList$.subscribe(
-            res => {
-                this.phonePrices = res.filter(x => x.model.includes(name));
-            }
+        this.getPrices(this.searcher);
+    }
+
+    countIndex(phone: PricesModel): number {
+        return (
+            this.phonePrices.indexOf(phone) + 1
+        ) + this.searcher.pageSize * (this.searcher.pageIndex - 1);
+    }
+
+    changeSite(pageIndex: number): void {
+        this.searcher.pageIndex = pageIndex;
+        this.getPrices(this.searcher);
+    }
+
+    private getPrices(searcher: SearcherModel = this.searcher): void {
+        this.pricesService.getPrices(searcher).subscribe(
+            (res: PricesApiGetPagModel) => {
+                // amount all objects getting api
+                this.phonesAmount = res.count;
+
+                // unnecessery values to setting pagination
+                this.searcher.pageIndex = res.pageIndex;
+
+                // default sorting is for producer,and is alphabetic
+                this.phonePrices = res.data;
+            },
+            (err: HttpErrorResponse) =>
+                this.toastr.error(err.error.message)
         );
     }
 }

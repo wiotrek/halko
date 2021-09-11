d7080d704e736901fef8a185179b536cb62bcc1f
wiotrek <piotrwaszak97@gmail.com>
2021-09-11 12:03:53 +0200

    #140 in progress

--- web/src/app/shared/components-specific/phones-extend/phones-extend.component.ts
+++ web/src/app/shared/components-specific/phones-extend/phones-extend.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output,
+    ViewChild,
+    TemplateRef
+} from '@angular/core';
 import { PhoneInListType } from '../../models-union/phone-in-list.type';
 import { RepairsModel } from '../../models/repairs.model';
 
@@ -10,4 +17,32 @@ import { RepairsModel } from '../../models/repairs.model';
 export class PhonesExtendComponent {
     @Input() elInList: PhoneInListType | RepairsModel;
     @Output() messageBack: EventEmitter<any> = new EventEmitter<any>();
+
+    @ViewChild(
+        'sellPhone', { static: true }
+    ) sellPhone: TemplateRef<any> | null = null;
+
+    @ViewChild(
+        'movePhone', { static: true }
+    ) movePhone: TemplateRef<any> | null = null;
+
+    choiceButton = true;
+    showBlock: TemplateRef<any> | null = null;
+
+    switchBlock(nameOfBlock: string): void {
+
+        switch (nameOfBlock) {
+            case 'sellPhone':
+                this.showBlock = this.sellPhone;
+                break;
+            case 'movePhone':
+                this.showBlock = this.movePhone;
+                break;
+            default:
+                this.showBlock = null;
+                break;
+        }
+
+        this.choiceButton = !this.choiceButton;
+    }
 }

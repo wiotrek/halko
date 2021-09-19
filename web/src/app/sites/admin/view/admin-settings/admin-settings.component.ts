e7077fedcbfdcc7b7f11e3f51037fc0cd64b7ab1
wiotrek <piotrwaszak97@gmail.com>
2021-09-19 15:55:33 +0200

    #183 creting form to add new point

--- web/src/app/sites/admin/view/admin-settings/admin-settings.component.ts
+++ web/src/app/sites/admin/view/admin-settings/admin-settings.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AdminService } from '../../admin.service';
 import { Links } from 'src/app/shared/models/links.model';
 
 @Component({
@@ -20,20 +19,10 @@ import { Links } from 'src/app/shared/models/links.model';
         <router-outlet></router-outlet>`
 })
 export class AdminSettingsComponent {
-    points: string[] = [];
-
     links: Links[] = [
         { caption: 'Punkty', path: 'punkty' },
         { caption: 'Pracownicy', path: 'pracownicy' },
         { caption: 'Admini', path: 'admini' },
         { caption: 'Statystyki', path: 'statystyki' }
     ];
-
-    constructor(private adminService: AdminService) {
-        this.getPointList();
-    }
-
-    getPointList(): void {
-        this.points = this.adminService.pointList;
-    }
 }
--- web/src/app/sites/admin/view/admin-settings/points-mgmt/points-mgmt.component.ts
+++ web/src/app/sites/admin/view/admin-settings/points-mgmt/points-mgmt.component.ts
@@ -1,7 +1,37 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../../admin.service';
+import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-points-mgmt',
-    template: 'punkty'
+    templateUrl: './points-mgmt.component.html',
+    styleUrls: ['./points-mgmt.component.scss']
 })
-export class PointsMgmtComponent {}
+export class PointsMgmtComponent {
+    pointCreatorMod = false;
+    pointsList: string[];
+
+    credentials: FormGroup;
+
+    constructor(private adminService: AdminService) {
+        this.pointsList = this.adminService.pointList;
+
+        this.credentials = new FormGroup({});
+        this.credentials.addControl('password', new FormControl('', [Validators.required]));
+        this.credentials.addControl('passwordConfirm', new FormControl(
+            '', [Validators.compose(
+                [Validators.required, this.validateAreEqual.bind(this)]
+            )]
+        ));
+    }
+
+    addNewPoint(f: NgForm): void {
+        console.log(this.credentials);
+    }
+
+    private validateAreEqual(fieldControl: FormControl): any {
+        return fieldControl.value === this.credentials.get('password').value ? null : {
+            NotEqual: true
+        };
+    }
+}

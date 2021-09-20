d271cd6650450d707d60fe81f7d1ab6966c95cf9
wiotrek <piotrwaszak97@gmail.com>
2021-09-20 21:41:21 +0200

    #183 creting form to add new point 2

--- web/src/app/sites/admin/view/admin-settings/points-mgmt/points-mgmt.component.ts
+++ web/src/app/sites/admin/view/admin-settings/points-mgmt/points-mgmt.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../../admin.service';
-import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-points-mgmt',
@@ -11,27 +11,11 @@ export class PointsMgmtComponent {
     pointCreatorMod = false;
     pointsList: string[];
 
-    credentials: FormGroup;
-
     constructor(private adminService: AdminService) {
         this.pointsList = this.adminService.pointList;
-
-        this.credentials = new FormGroup({});
-        this.credentials.addControl('password', new FormControl('', [Validators.required]));
-        this.credentials.addControl('passwordConfirm', new FormControl(
-            '', [Validators.compose(
-                [Validators.required, this.validateAreEqual.bind(this)]
-            )]
-        ));
     }
 
     addNewPoint(f: NgForm): void {
-        console.log(this.credentials);
-    }
-
-    private validateAreEqual(fieldControl: FormControl): any {
-        return fieldControl.value === this.credentials.get('password').value ? null : {
-            NotEqual: true
-        };
+        this.adminService.addNewPoint(f.value);
     }
 }

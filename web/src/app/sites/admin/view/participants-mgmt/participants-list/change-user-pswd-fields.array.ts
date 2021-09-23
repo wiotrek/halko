5cfc0a238a520f7421d562ccbc40039f685e3272
wiotrek <piotrwaszak97@gmail.com>
2021-09-23 22:09:39 +0200

    close #202

---
+++ web/src/app/sites/admin/view/participants-mgmt/participants-list/change-user-pswd-fields.array.ts
@@ -0,0 +1,22 @@
+import { PhoneFieldsModel } from 'src/app/shared/models/phone-fields.model';
+
+export const ChangeUserPswdFieldsArray: PhoneFieldsModel[] = [
+    {
+        category: 'login',
+        polishName: 'Nazwa uzytkownika',
+        isNumber: false,
+        required: true
+    },
+    {
+        category: 'currentPassword',
+        polishName: 'Aktualne hasło',
+        isNumber: false,
+        required: true
+    },
+    {
+        category: 'newPassword',
+        polishName: 'Nowe haslo',
+        isNumber: false,
+        required: true
+    }
+];
